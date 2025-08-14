5d0f35b2bbdc002ee478691d0e8a1d937b8340cd
ShubhamPandey77 <shubhampandeys084@gmail.com>
2025-08-14 18:50:04 +0530

    Forms and input basic

---
+++ 17FormsAndInputs/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/HEader";
+import Login from "./Components/Login";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
